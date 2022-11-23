8eed33acd47afbe14d960f0b620ae764080ce576
刘江江 <liu_jiangjiang@csisecurity.com.cn>
2022-11-23 13:38:44 +0800

    111

--- src/main/java/com/wdbyte/bing/Wallpaper.java
+++ src/main/java/com/wdbyte/bing/Wallpaper.java
@@ -1,6 +1,8 @@
 package com.wdbyte.bing;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -48,7 +50,65 @@ public class Wallpaper {
         BingFileUtils.writeBing(imagesList);
         BingFileUtils.writeReadme(imagesList);
         BingFileUtils.writeMonthInfo(imagesList);
+        imagesList.forEach(ele -> {
+            try {
+                downLoad(ele.getUrl(),ele.getDate(),ele.getDate());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
         new WebSiteGenerator().htmlGenerator();
+    }
+
+    public static void downLoad(String sUrl, String name, String datetime) throws Exception {
+        //定义一个URL对象，就是你想下载的图片的URL地址
+        URL url = new URL(sUrl);
+        //打开连接
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        //设置请求方式为"GET"
+        conn.setRequestMethod("GET");
+        //超时响应时间为10秒
+        conn.setConnectTimeout(10 * 1000);
+        //通过输入流获取图片数据
+        InputStream is = conn.getInputStream();
+        //得到图片的二进制数据，以二进制封装得到数据，具有通用性
+        byte[] data = readInputStream(is);
+        //创建一个文件对象用来保存图片，默认保存当前工程根目录，起名叫Copy.jpg
+        datetime = datetime.substring(0, 7);
+        String pathName = "D:\\wallpaper" +"\\" + datetime + "\\"+ name +".jpg";
+        File file = new File(pathName);
+        if (file.getParentFile() != null && !file.getParentFile().exists()){
+            file.getParentFile().mkdirs();
+        }
+        if (!file.exists()) {
+            boolean newFile = file.createNewFile();
+        } else {
+            file.delete();
+            file.createNewFile();
+        }
+        //创建输出流
+        FileOutputStream outStream = new FileOutputStream(file);
+        //写入数据
+        outStream.write(data);
+        //关闭输出流，释放资源
+        outStream.close();
+    }
+
+    public static byte[] readInputStream(InputStream inStream) throws Exception {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        //创建一个Buffer字符串
+        byte[] buffer = new byte[6024];
+        //每次读取的字符串长度，如果为-1，代表全部读取完毕
+        int len;
+        //使用一个输入流从buffer里把数据读取出来
+        while ((len = inStream.read(buffer)) != -1) {
+            //用输出流往buffer里写入数据，中间参数代表从哪个位置开始读，len代表读取的长度
+            outStream.write(buffer, 0, len);
+        }
+        //关闭输入流
+        inStream.close();
+        //把outStream里的数据写入内存
+        return outStream.toByteArray();
     }
 
 }
